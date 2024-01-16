d9000b524efbafad7d961f75ca7e81bf28a697e7
TuPhiAnh <phianhtu2211@gmail.com>
2024-01-16 23:48:33 +0700

    initial project

---
+++ src/test/java/vn/com/tech/awesome/job/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.com.tech.awesome.job.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
