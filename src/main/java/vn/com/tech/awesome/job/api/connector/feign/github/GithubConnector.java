39d2d43384fcb9ab8abd40175bb4ae1899ba1475
AnhTuPhi <phianhtu2211@gmail.com>
2024-01-17 00:52:30 +0700

    update API get issues github repo

--- src/main/java/vn/com/tech/awesome/job/api/connector/feign/github/GithubConnector.java
+++ src/main/java/vn/com/tech/awesome/job/api/connector/feign/github/GithubConnector.java
@@ -1,6 +1,9 @@
 package vn.com.tech.awesome.job.api.connector.feign.github;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * @author : AnhTuPhi
@@ -9,4 +12,13 @@ import org.springframework.cloud.openfeign.FeignClient;
  **/
 @FeignClient(name = "github", url = "${github.restapi}")
 public interface GithubConnector {
+
+    @GetMapping("/repos/{owner}/{repo}/issues")
+    Object getIssues(@PathVariable("owner") String owner,
+                     @PathVariable("repo") String repo,
+                     @RequestParam("per_page") Integer perPage,
+                     @RequestParam("page") Integer page,
+                     @RequestParam("state") String state,
+                     @RequestParam("sort") String sort,
+                     @RequestParam("since") String since);
 }
